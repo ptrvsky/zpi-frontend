dc53caaed7eb9d83bff33ef931908ecad1223d7d
Peterovsky <Peterovsky7@gmail.com>
2020-04-02 15:48:15 +0200

    Updates

---
+++ precache-manifest.0f43bdabab4930424674960aec0b7791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fb420776d4fc62a7a2443487d4ea0a9",
+    "url": "/zpi-frontend/index.html"
+  },
+  {
+    "revision": "fc0a4c7bf0898d68623e",
+    "url": "/zpi-frontend/static/css/main.b525de3c.chunk.css"
+  },
+  {
+    "revision": "cb023886c84795eabf9c",
+    "url": "/zpi-frontend/static/js/2.56b71c20.chunk.js"
+  },
+  {
+    "revision": "5ab1baa52e351a54fb487f180027ee45",
+    "url": "/zpi-frontend/static/js/2.56b71c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0a4c7bf0898d68623e",
+    "url": "/zpi-frontend/static/js/main.6ddf080a.chunk.js"
+  },
+  {
+    "revision": "59c9e68597c1c2c79252",
+    "url": "/zpi-frontend/static/js/runtime-main.e93567c9.js"
+  }
+]);
