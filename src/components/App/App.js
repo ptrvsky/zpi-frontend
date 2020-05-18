e84bff8f09ec0812b0c13ad845de77d86d4d8348
Peterovsky <Peterovsky7@gmail.com>
2020-05-18 15:24:31 +0200

    Set up cart component and route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,6 +8,7 @@ import Menu from '../Menu/Menu';
 import About from '../About/About';
 import Contact from '../Contact/Contact';
 import Login from '../Login/Login';
+import Cart from '../Cart/Cart';
 
 import './App.scss';
 
@@ -26,6 +27,7 @@ function App() {
             <Route path="/about" exact component={About} />
             <Route path="/contact" exact component={Contact} />
             <Route path="/login" component={Login} />
+            <Route path="/cart" component={Cart} />
 
           </div>
         </div>
---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../../assets/styles/buttons.scss';
+import './Cart.scss';
+
+export default class Cart extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {
+    }
+  }
+
+  componentDidMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="cart-wrapper">
+        <div className="cart-content-wrapper">
+
+        </div>
+      </div>
+    );
+  }
+}
