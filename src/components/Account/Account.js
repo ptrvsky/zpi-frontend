7af3a2ce047f8de1f71696167dcb5e267c389a0a
Peterovsky <Peterovsky7@gmail.com>
2020-06-07 23:38:35 +0200

    Add user current data to account subpage

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -8,6 +8,12 @@ export default class Account extends React.Component {
     super(props);
     this.state = {
       error: null,
+      userData: {
+        email: null,
+        number: null,
+        phone: null,
+        street: null,
+      }
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -15,18 +21,26 @@ export default class Account extends React.Component {
 
   componentDidMount() {
 
-    let user = null
-    if (localStorage.getItem('user')) user = JSON.parse(localStorage.getItem('user'));
-    if (!user) {
-      this.props.history.push('/');
+    if (!localStorage.getItem('user')) {
+      this.props.history.push('/login?unauthenticatedAccessTry=true');
+      return;
     }
+
+    fetch('https://pizzeria-backend-zpi.herokuapp.com/v1/user/userAddress', {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${JSON.parse(localStorage.getItem('user')).token}`
+      }
+    })
+      .then(userData => userData.json())
+      .then(userData => this.setState({ userData }));
 
   }
 
   handleSubmit(event) {
     event.preventDefault(); // Prevent browser reload/refresh
 
-    fetch('https://pizzeria-backend-zpi.herokuapp.com/v1/account', {
+    fetch('https://pizzeria-backend-zpi.herokuapp.com/v1/user/address', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
@@ -36,8 +50,7 @@ export default class Account extends React.Component {
         street: event.target[0].value,
         number: event.target[1].value,
         phone: event.target[2].value,
-        mail: event.target[3].value,
-        password: event.target[4].value,
+        email: event.target[3].value,
       })
     })
       .then(res => {
@@ -67,20 +80,21 @@ export default class Account extends React.Component {
               <div className="form-fields-wrapper">
                 <div className="form-element form-element--street">
                   <label>Ulica</label>
-                  <input type="text" name="street" />
+                  <input type="text" name="street" defaultValue={this.state.userData.street ? this.state.userData.street : null} />
                 </div>
                 <div className="form-element form-element--number">
                   <label>Numer lokalu</label>
-                  <input type="text" name="number" />
+                  <input type="text" name="number" defaultValue={this.state.userData.number ? this.state.userData.number : null} />
                 </div>
                 <div className="form-element">
                   <label>Numer telefonu</label>
-                  <input type="text" name="phone" />
+                  <input type="text" name="phone" defaultValue={this.state.userData.phone ? this.state.userData.phone : null} />
                 </div>
                 <h1><br />Dane konta</h1>
                 <div className="form-element">
                   <label>E-mail</label>
-                  <input type="email" name="email" placeholder="Podaj swój adres e-mail" autoComplete="new-password" />
+                  <input type="email" name="email" placeholder="Podaj swój adres e-mail" autoComplete="new-password"
+                    defaultValue={JSON.parse(localStorage.getItem('user')) ? JSON.parse(localStorage.getItem('user')).mail : null} />
                 </div>
                 <div className="form-element">
                   <label>Hasło</label>
