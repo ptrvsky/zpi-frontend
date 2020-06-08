7a9428f5126e1c47bf849da1514bc37f390ab9c4
Peterovsky <Peterovsky7@gmail.com>
2020-06-08 22:04:25 +0200

    Add cook and delivery view links to user menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -59,6 +59,8 @@ class NavigationBar extends React.Component {
                   <div className="dropdown-item disabled">{this.state.user.mail}</div>
                   <NavDropdown.Divider />
                   <Link to="/orders" className="dropdown-item">Zamowienia</Link>
+                  <Link to="/cook_view" className="dropdown-item">Widok kucharza</Link>
+                  <Link to="/delivery_view" className="dropdown-item">Widok dostawcy</Link>
                   <Link to="/cart" className="dropdown-item">Koszyk</Link>
                   <Link to="/account" className="dropdown-item">Dane konta</Link>
                   <div className="dropdown-item" onClick={this.handleLogout}>Wyloguj</div>
@@ -87,6 +89,8 @@ class NavigationBar extends React.Component {
                   <div className="dropdown-item disabled">{this.state.user.mail}</div>
                   <NavDropdown.Divider />
                   <Link to="/orders" className="dropdown-item">Zamowienia</Link>
+                  <Link to="/cook_view" className="dropdown-item">Widok kucharza</Link>
+                  <Link to="/delivery_view" className="dropdown-item">Widok dostawcy</Link>
                   <Link to="/account" className="dropdown-item">Dane konta</Link>
                   <div className="dropdown-item" onClick={this.handleLogout}>Wyloguj</div>
                 </NavDropdown>
