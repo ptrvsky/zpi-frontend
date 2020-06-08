43682f042398eca12d42350465405ada2230ad69
Katsiaryna Udanouskaya <polar_bumblebee@linux.home>
2020-06-08 21:19:27 +0200

    Get data for CookView

--- src/components/CookView/CookView.js
+++ src/components/CookView/CookView.js
@@ -1,71 +1,68 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../assets/styles/buttons.scss';
 import OrderElement from './OrderElement/OrderElement';
 import './CookView.scss';
+import './OrderElement/OrderElement.scss';
 
 export default class CookView extends React.Component {
 
-  constructor(props) {
-    super();
-    this.state = {
-      pizzas: [
-        {
-          "id":1,
-          "typeName": "202020",
-          "ingredients": [
-            {
-              "id": 2,
-              "name": "Cheese",
-              "price": 4.5,
-              "ifAllergen": true,
-              "quantity": 1
-            }
-          ],
-          "diameter": "19.03   21:57",
-          "crust": "Skadnik 1, skaldnik 2, ciasto 20 cm"
-        },
-        {
-          "id":3,
-          "typeName": "303030",
-          "ingredients": [
-            {
-              "id": 4,
-              "name": "HamHa",
-              "price": 4.5,
-              "ifAllergen": false,
-              "quantity": 1
-            }
-          ],
-          "diameter": "20.03   00:09",
-          "crust": "Skadnik 3, skaldnik 2, ciasto 30 cm"
-        }
-      ],
-      
-    }
-  }
-  
+    constructor(props) {
+        super();
+        this.state = {
+            orders: [],
+            pizzas: [],
+            message: null,
 
-  render() {
-    return (
-      <div className="menu-wrapper">
-        <div className="menu-content-wrapper">
-            <div className="menu-content">
-            <div className="menu-title">CookView</div>
-            <div className="menu-title-wrapper">
-              <div className="id-element">Lp.</div>
-              <div className="name-element">Nr Zamowienia</div>
-              <div className="ingredients-title">Skład</div>
-              <div className="crust-element">Czas zamowienia</div>
-              <div className="button-element">Status</div>
-              </div>
+        }
+    }
+
+    fetchOrders() {
+        let url = 'https://pizzeria-backend-zpi.herokuapp.com/v1/view/cook';
+
+        fetch(url)
+            .then(response => response.json())
+            .catch(err => console.log(err));
+    }
+
+    fetchPizzas() {
+        let url = 'https://pizzeria-backend-zpi.herokuapp.com/v1/pizza';
+
+        fetch(url)
+            .then(response => response.json())
+            .catch(err => console.log(err));
+    }
+
+    componentDidMount() {
+        this.fetchOrders();
+        this.fetchPizzas();
+      }
+    
+
+    render() {
+        return (
+            <div className="menu-wrapper">
+                <div className="menu-content-wrapper">
+                    <div className="menu-content">
+                        <div className="menu-title">Zamowienia pizz</div>
+                        <div className="menu-title-wrapper">
+                            <div className="id-element">Lp.</div>
+                            <div className="name-element">Nr Zamowienia</div>
+                            <div className="ingredients-title">Skład</div>
+                            <div className="time-element">Czas zamowienia</div>
+                            <div className="button-element">Status</div>
+                        </div>
+                    </div>
+                    {this.state.orders.map((order, index) => {
+                          console.log(this.state.index);
+                        return <OrderElement details={order} key={order.id} index={index} />                       
+
+                    })
+                    }
+
+                </div>
             </div>
-            {this.state.pizzas ?
-              this.state.pizzas.map(pizza => <OrderElement details={pizza} key={pizza.id} />) :
-              null}
-          </div>
-        </div>
-      );
-  }
+        );
+    }
 }
 
-  
--- src/components/CookView/OrderElement/OrderElement.js
+++ src/components/CookView/OrderElement/OrderElement.js
@@ -4,23 +4,24 @@ import './OrderElement.scss';
 
 export default class OrderElement extends React.Component {
 
-  handleOrderClick() {
-    // TO-DO: Add selected pizza to cart
-  }
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
 
-  render() {
-    return (
-<div className="menu-element-wrapper">
-        <div className="id-element">{this.props.details.id}</div>
-        <div className="name-element">{this.props.details.typeName}</div>
-        <div className="ingridients-element">{this.props.details.crust}</div>
-        <div className="crust-element">{this.props.details.diameter}</div>
-        <label class="input-check"><input onchange="change_state(this)" type="checkbox" value="something" name="test"/> Wykonanie </label>
-        </div>
+    render() {
+        return (
+            <div className="menu-element-wrapper">
+                <div className="id-element">{this.props.index+1}</div>
+                <div className="name-element">{this.props.details.orderId}</div>   
+                <div className="ingridients-element">{this.props.details.pizzaIngredientsDto}</div>
+                <div className="data-element">{this.props.details.date}</div>
+                <label class="input-check"><input onchange="change_state(this)" type="checkbox" value="something" name="test" /> Wykonanie </label>
+            </div>
 
-        
-    );
-  }
+
+        );
+    }
 }
 
 
