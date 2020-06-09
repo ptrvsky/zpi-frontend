b98b57203613b0ba1f9dc17f4084a4dd39ab89e5
Peterovsky <Peterovsky7@gmail.com>
2020-06-09 09:48:35 +0200

    Fix issues on cook view subpage

--- src/components/CookView/OrderElement/OrderElement.js
+++ src/components/CookView/OrderElement/OrderElement.js
@@ -7,20 +7,34 @@ export default class OrderElement extends React.Component {
     constructor(props) {
         super(props);
         this.state = {};
+        this.changeStatus = this.changeStatus.bind(this);
+    }
+
+    changeStatus() {
+        fetch('/v1/order/advance', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                id: this.props.details.orderId,
+                newOrderStatus: "W DRODZE"
+            })
+        })
     }
 
     render() {
+        let date = new Date(this.props.details.date).toISOString();
         return (
             <div className="menu-element-wrapper">
                 <div className="id-element">{this.props.index + 1}</div>
                 <div className="name-element">{this.props.details.orderId}</div>
-                <div className="ingridients-element">{this.props.details.pizzas[0] ? this.props.details.pizzas.map((pizzaIngredient, index) =>
-                    pizzaIngredient.name + (index < this.props.details.pizzas.length - 1 ? ', ' : '')
-                ) : null}</div>
+                <div className="ingridients-element"><ul>{this.props.details.pizzas[0] ? this.props.details.pizzas.map((pizza) =>
+                    <li>{pizza.pizzaIngredientDto.map((pizzaIngredient, index) => pizzaIngredient.name + (index < pizza.pizzaIngredientDto.length - 1 ? ', ' : ''))}</li>
+                ) : null}</ul></div>
 
-
-                <div className="data-element">{this.props.details.date}</div>
-                <label class="input-check"><input onchange="change_state(this)" type="checkbox" value="something" name="test" /> Wykonanie </label>
+                <div className="data-element">{date.slice(0, 10) + ", " + date.slice(11, 16)}</div>
+                <label class="input-check"><input onChange={this.changeStatus} type="checkbox" value="something" name="test" /> Wykonanie </label>
             </div>
 
 
